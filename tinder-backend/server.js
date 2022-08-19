eb5b72174a6be68030844a96b036a6b91c2d62f5
Rajiv Ramachandra Nidumolu <39266394+RajivRamachandraNidumolu@users.noreply.github.com>
2022-08-19 14:27:50 -0500

    Add files via upload

---
+++ tinder-backend/server.js
@@ -0,0 +1,55 @@
+import express from "express";
+import mongoose from "mongoose";
+import Cards from './dbCards.js' ;
+import Cors from 'cors';
+
+//App config
+const app= express();
+const port = process.env.PORT || 8001
+const connection_url= 'mongodb+srv://admin:gUswXFcnY3CWhExU@cluster0.twrey3x.mongodb.net/?retryWrites=true&w=majority'
+
+//Middlewares
+app.use(express.json());
+app.use(Cors());
+
+//DB config
+mongoose.connect(connection_url)
+
+// API Endpoints
+app.get('/',(req,res) => res.status(200).send('Hello world'));
+app.post('/tinder/cards',(req,res) => {
+    const dbCard= req.body;
+
+    Cards.create(dbCard,(err,data) => {
+        if(err)
+        {
+            res.status(500).send(err)
+        }
+
+        else{
+            res.status(201).send(data)
+        }
+
+    })
+    
+});
+
+app.get('/tinder/cards',(req,res) => {
+
+    Cards.find((err,data) => {
+        if(err)
+        {
+            res.status(500).send(err)
+        }
+
+        else{
+            res.status(200).send(data)
+        }
+
+    })
+    
+});
+
+
+//Listener
+app.listen(port,() => console.log(`listening on localhost:${port}`))
